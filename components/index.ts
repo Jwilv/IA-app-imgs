26e4346a017c594fbd3cbabe837f1828f0cda72f
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-26 18:13:42 -0300

    home init

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,7 @@
 export * from './index';
 export * from './layouts';
+export * from './ui/card';
+export * from './ui/formField';
 export * from './ui';
+export * from './ui/loader';
 export * from './ui/navbar';
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,2 +1,5 @@
+export * from './card';
+export * from './formField';
 export * from './index';
+export * from './loader';
 export * from './navbar';
