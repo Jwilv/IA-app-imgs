8df27b67d905b859395a6a30fb584a79d5f358b7
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-26 21:06:18 -0300

    form

--- components/ui/formField/FormField.tsx
+++ components/ui/formField/FormField.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
-const FormField = () => {
+interface Props {
+  labelName: string,
+  type: string,
+  name: string,
+  placeholder: string,
+  value: string,
+  handleChange: any,
+  isSurpriseMe?: boolean,
+  handleSurpriseMe?: any
+}
+
+const FormField = (field: Props) => {
   return (
-    <div>FormField</div>
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label htmlFor={field.name} className='block text-sm font-medium text-gray-900'>
+          {field.labelName}
+        </label>
+        {field.isSurpriseMe && (
+          <button
+            type='button'
+            onClick={field.handleSurpriseMe}
+            className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input
+        type={field.type}
+        id={field.name}
+        name={field.name}
+        placeholder={field.placeholder}
+        value={field.value}
+        onChange={field.handleChange}
+        required
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+        focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3'
+      />
+    </div>
   )
 }
 
