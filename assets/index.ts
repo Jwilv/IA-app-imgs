885633467716714be1bbdebec72932e5c9e28367
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-30 16:11:43 -0300

    update navbar

--- assets/index.ts
+++ assets/index.ts
@@ -1,5 +1,5 @@
 import download from './download.png';
-import logo from './logo.svg';
+import logo from './ie.jpg';
 import preview from './preview.png';
 
 export {
--- components/ui/navbar/navbar.tsx
+++ components/ui/navbar/navbar.tsx
@@ -6,14 +6,13 @@ import React from 'react'
 const Navbar = () => {
     return (
         <header
-            className=' w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'
+            className=' w-full flex justify-start items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] gap-5' 
         >
-            <Link href={'/'}>
-                <Image
-                    src={logo}
-                    alt='logo page'
-                    className='w-28 object-contain'
-                />
+            <Link 
+            href={'/'}
+             className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'
+            >
+                Comunity
             </Link>
 
             <Link
